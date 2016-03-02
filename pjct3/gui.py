ef77af923fb51b10f19c908b524ea2253a30a7ba
kittleik <haavarki@stud.ntnu.no>
2016-03-02 14:49:37 +0100

    heihei

--- pjct3/gui.py
+++ pjct3/gui.py
@@ -1,12 +1,62 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.9.1') # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from kivy.properties import ListProperty
+
+#kivy Config
+from kivy.config import Config
+Config.set('graphics', 'height','1000')
+Config.set('graphics', 'width','1200')
+
+class Agent(Widget):
+
+    def __init__(self, **kwargs):
+        super(Agent, self).__init__(**kwargs)
+
+        with self.canvas.before:
+            Color(0,1,0)
+            Rectangle(size=(100,100),
+                        pos=self.pos)
+
+
+class FlatLand(Widget):
+
+    itemIndex = [[0]*10]*10
+
+    def __init__(self, **kwargs):
+        super(FlatLand, self).__init__(**kwargs)
+
+        with self.canvas.before:
+            Rectangle(source='img/flatland.png',size=(1000, 1000))
+
+        agent = Agent()
+
+        self.add_widget(agent)
+
+        agent.moveUp()
+
+class ControlPanel(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ControlPanel, self).__init__(**kwargs)
+        btn1 = Button(text="^")
+
+        self.add_widget(btn1)
+        self.add_widget(Button(text="v"))
+        self.add_widget(Button(text=">"))
+        self.add_widget(Button(text="<"))
 
 class RootWidget(BoxLayout):
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
+        self.add_widget(FlatLand())
+        self.add_widget(ControlPanel(orientation='vertical',
+                                        size_hint=(.2,1)))
+
 
 class GUI(App):
     def build(self):
--- pjct3/main.py
+++ pjct3/main.py
@@ -1,4 +1,4 @@
-import GUI from gui
+from gui import GUI
 
 if __name__ == '__main__':
-    TestApp().run()
+    GUI().run()
